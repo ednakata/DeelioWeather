c36118730d1de4519b418177581f9f42bd879785
Ed Nakata <ednakata.dev@gmail.com>
2013-01-01 18:38:41 -0800

    RoughApp

--- src/com/homeserver/edn/deelioweather/MainActivity.java
+++ src/com/homeserver/edn/deelioweather/MainActivity.java
@@ -1,11 +1,16 @@
 package com.homeserver.edn.deelioweather;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
 
+	public final static String EXTRA_MESSAGE = "com.homeserver.edn.deelioweather.MESSAGE";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -19,4 +24,12 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	public void submitZipCode(View view) {
+		//Do something in response to the click of the submit button
+		Intent intent = new Intent(this, DisplayWeatherActivity.class);
+		EditText zipCodeText = (EditText) findViewById(R.id.zipCode);
+		String message = zipCodeText.getText().toString();
+		intent.putExtra(EXTRA_MESSAGE, message);
+		startActivity(intent);
+	}
 }
